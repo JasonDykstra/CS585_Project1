00ef73023d573ef7ecd01098c5c61ecbdb1c7606
jasondykstra <jasondykstra2000@gmail.com>
2022-10-03 15:49:49 -0400

    added code for query1, converted to maven project

---
+++ src/TestQueries.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class TestQueries {
+
+
+    @Test
+    public void debug() throws Exception{
+        // Problem 1
+        String[] input = new String[2];
+        input[0] = "hdfs://localhost:9000/root/data/customers.csv";
+        input[1] = "hdfs://localhost:9000/root/data/transactions.csv";
+
+        // Run map reduce job
+
+
+    }
+}
